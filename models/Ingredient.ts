35a9042a9e15ba5ee31a96bbae977238e7c448cf
Jordan Christley <lilj390150@live.com>
2016-12-30 12:17:06 -0600

    I changed the ingredients model and added a text index

--- models/Ingredient.ts
+++ models/Ingredient.ts
@@ -1,21 +1,24 @@
 import * as mongoose from 'mongoose';
 
 export interface IIngredient extends mongoose.Document {
-    name: string,
+    names: string[],
     foods: string[],
     definition: string,
-    organic: boolean,
+    description: string,
     adminTags: string[],
     userTags: string[]
 }
 
 let ingredientSchema = new mongoose.Schema({
-    name: {type: String, required: true},
+    names: {type: [String], required: true},
     foods: {type: [String], required: true, default: []},
     definition: String,
-    organic: Boolean,
+    description: String,
     adminTags: [String],
     userTags: [String]
 });
+
+//This is the easiest way to make the ingredient's names field searchable.
+ingredientSchema.index({names: 'text'});
 
 export default mongoose.model<IIngredient>('Ingredient', ingredientSchema);
